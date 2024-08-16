6a887904b4ff87c123aca16cb82fb0cd3e077b3d
JorgeTerence <jorge.terence@outlook.com>
2024-08-16 18:51:23 +0000

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import shake from './shake.jfif'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <img src={shake} className="w-40" alt="logo" />
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
