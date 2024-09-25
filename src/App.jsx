4a1575e482634c3f5b95267d17e9c602dbafbfc5
Jorge Terence <jorge.terence@outlook.com>
2024-09-25 20:48:42 +0000

    linked to book api

--- src/App.jsx
+++ src/App.jsx
@@ -5,39 +5,15 @@ import Navbar from "./components/Navbar";
 import ProductCard from "./components/ProductCard";
 
 function App() {
-  const products = [
-    {
-      name: "Café preto",
-      price: 5,
-      discount: 0,
-      picture:
-        "https://assets.delirec.com/images%2FezvX2clyQIYiWF3AAjQDXT2Xt6q2%2Frecipe%2Feb175f30-1278-4944-adcd-b8974ada7853-Caf%C3%A9--preto.-gallery-0",
-    },
-    {
-      name: "Pom na chapa",
-      price: 4,
-      discount: 0,
-      picture:
-        "https://conteudo.imguol.com.br/c/entretenimento/ff/2021/09/29/padocaria-sp--pao-na-chapa-1632933532062_v2_1x1.jpg",
-    },
-    {
-      name: "Shake de maracujá",
-      price: 16,
-      discount: 0.2,
-      picture:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpV-QjOSULXZhq1i5GyGLMRo0Hw9T_aOt4bA&s",
-    },
-  ];
-
-  const API_URL = "https://fishnet-api-py.onrender.com/itens";
+  const API_URL = "https://shake-s-peare.onrender.com/books";
   const [produtos, setProdutos] = useState([]);
-  const [coffeeShop, setCoffeeShop] = useState(products);
+  const [coffeeShop, setCoffeeShop] = useState([]);
   const [firstN, setFirstN] = useState(5);
 
   function carregaProdutos() {
     axios.get(API_URL).then((res) => {
       setProdutos(res.data);
-      setCoffeeShop(() => [...products, ...res.data.filter((prod) => prod.temporary)]);
+      setCoffeeShop(() => res.data);
       console.log(res.data);
     });
   }
@@ -47,10 +23,11 @@ function App() {
     const form = e.target;
     const data = new FormData(form);
     const produto = {
-      name: data.get("name"),
-      price: data.get("price"),
-      picture: data.get("picture"),
-      temporary: true,
+      title: data.get("title"),
+      author: data.get("author"),
+      description: data.get("description"),
+      rating: data.get("rating"),
+      picture: data.get("picture") ?? "sem-imagem",
     };
 
     axios.post(API_URL, produto).then((res) => {
@@ -73,8 +50,12 @@ function App() {
         </h2>
 
         <section className="flex gap-8 mb-10">
-          {coffeeShop.map((prod, i) => (
-            <ProductCard prod={prod} carregaProdutos={carregaProdutos} key={i} />
+          {produtos.map((prod, i) => (
+            <ProductCard
+              prod={prod}
+              carregaProdutos={carregaProdutos}
+              key={i}
+            />
           ))}
         </section>
 
@@ -113,25 +94,6 @@ function App() {
             Adicionar produto
           </button>
         </form>
-
-        <h2 className="text-lg font-semibold">
-          Primeiros{" "}
-          <input
-            type="number"
-            value={firstN}
-            onChange={(e) => setFirstN(e.target.value)}
-            className="w-10 px-2 rounded-lg shadow-sm border border-stone-300"
-            style={{ "-moz-appearance": "textfield" }}
-          />{" "}
-          produtos da API FishNet usada no nosso TCC:
-        </h2>
-        <ul className="list-disc ml-5">
-          {produtos.slice(0, firstN).map((prod, i) => (
-            <li key={i}>
-              {prod.name}, {prod.price}
-            </li>
-          ))}
-        </ul>
       </main>
     </div>
   );
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,32 +1,30 @@
 import axios from "axios";
 import { price } from "../lib/format";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default ({ prod, carregaProdutos }) => {
-  function remover (id){
+  function remover(id) {
     let api = "https://fishnet-api-py.onrender.com/itens";
-    axios.delete(`${api}/${id}`)
-      .then(()=>{
-
-      })
+    axios.delete(`${api}/${id}`).then(() => {
+      carregaProdutos();
+    });
   }
+
   return (
-    <div className="border border-slate-400 shadow rounded-lg">
+    <div className="border border-slate-400 shadow rounded-lg w-52">
       <img src={prod.picture} alt={prod.name} className="w-40 rounded-t-lg" />
 
       <div className="p-3">
-        {prod.discount ? (
-          <div className="flex flex-col">
-            <span className="font-bold text-lg">
-              {price(prod.price * (1 - prod.discount))}
-            </span>
-            <span className="line-through text-stone-500 text-sm">
-              {price(prod.price)}
-            </span>
-          </div>
-        ) : (
-          <span className="font-bold text-lg">{price(prod.price)}</span>
-        )}
-        <p>{prod.name}</p>
+        
+        <img src={prod.cover} alt={`Capa do livro: ${prod.title}`} />
+        <p className="truncate font-bold">{prod.title}</p>
+        <p className="truncate">{prod.author}</p>
+        <button
+          onClick={remover}
+          className="rounded-lg bg-indigo-400 border border-indigo-700 shadow-sm w-full py-1 mt-2"
+        >
+          Exlcuir
+        </button>
       </div>
     </div>
   );
